8a9c2d97bd836cb11f19f1725bc83a90077b0cdf
josh sylvester <josh.sylvester@ge.com>
2018-12-13 14:02:52 -0800

    UI-2984 fixing getHooks

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -91,7 +91,7 @@ const plugins = [
   // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
   // In production, it will be an empty string unless you specify "homepage"
   // in `package.json`, in which case it will be the pathname of that URL.
-  new InterpolateHtmlPlugin(env.raw),
+  new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
   // Generates an `index.html` file with the <script> injected.
   new HtmlWebpackPlugin({
     template: paths.appHtml,
